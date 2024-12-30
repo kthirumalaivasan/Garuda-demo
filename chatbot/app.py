393c2f363ead5b53cfc92cdc5811c3c8b4eed3b4
Thirumalaivasan K <thiru@iqtechmax.com>
2024-12-30 18:20:20 +0530

    disabled debugger mode

--- chatbot/app.py
+++ chatbot/app.py
@@ -27,4 +27,4 @@ def chat_route():
 logger.info("Garuda Demo bot started successfully")
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
